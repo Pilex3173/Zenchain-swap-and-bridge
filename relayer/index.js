be10e7d643e4ebaaa5c7aea8e54e7d3e27cead80
Rahmad Ardi Handoyo <158312970+Pilex3173@users.noreply.github.com>
2025-07-07 22:16:17 +0700

    Create index.js

---
+++ relayer/index.js
@@ -0,0 +1,23 @@
+const { ethers } = require("ethers");
+const senderAbi = [ "event TokenLocked(address,uint256,address)" ];
+const providerZen = new ethers.providers.JsonRpcProvider("https://rpc.zencahain.testnet");
+const providerSep = new ethers.providers.JsonRpcProvider("https://rpc.sepolia.testnet");
+const bridgeSenderAddr = "ALAMAT_BRIDGE_SENDER";
+const bridgeReceiverAddr = "ALAMAT_BRIDGE_RECEIVER";
+const ethTokenAddr = "0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43";
+const relayerPrivKey = "0xYOUR_KEY";
+
+const wallet = new ethers.Wallet(relayerPrivKey, providerSep);
+const receiver = new ethers.Contract(bridgeReceiverAddr, [
+  "function releaseETH(address to, uint256 amount) external"
+], wallet);
+
+(async () => {
+  const zs = new ethers.Contract(bridgeSenderAddr, senderAbi, providerZen);
+  zs.on("TokenLocked", async (user, amount, targetAddress) => {
+    console.log("Locked:", user, amount.toString(), targetAddress);
+    const tx = await receiver.releaseETH(targetAddress, amount);
+    console.log("Released tx:", tx.hash);
+  });
+  console.log("Relayer running...");
+})();
