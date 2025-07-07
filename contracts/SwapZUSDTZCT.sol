beb4b8a1d77a14ceb3e55f1d2f948e3b9612677a
Rahmad Ardi Handoyo <158312970+Pilex3173@users.noreply.github.com>
2025-07-08 02:16:22 +0700

    Update SwapZUSDTZCT.sol

--- contracts/SwapZUSDTZCT.sol
+++ contracts/SwapZUSDTZCT.sol
@@ -1,28 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 interface IERC20 {
-    function transferFrom(address, address, uint256) external returns (bool);
-    function transfer(address, address, uint256)   external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transfer(address recipient, uint256 amount) external returns (bool);
 }
+
 contract SwapZUSDTZCT {
     address public owner;
     IERC20 public zusdt;
     IERC20 public zct;
+
     constructor(address _zusdt, address _zct) {
         owner = msg.sender;
         zusdt = IERC20(_zusdt);
-        zct   = IERC20(_zct);
+        zct = IERC20(_zct);
     }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+
     function swapZUSDTtoZCT(uint256 amount) external {
         require(zusdt.transferFrom(msg.sender, address(this), amount), "ZUSDT transfer failed");
         require(zct.transfer(msg.sender, amount), "ZCT transfer failed");
     }
+
     function swapZCTtoZUSDT(uint256 amount) external {
         require(zct.transferFrom(msg.sender, address(this), amount), "ZCT transfer failed");
         require(zusdt.transfer(msg.sender, amount), "ZUSDT transfer failed");
     }
-    function withdrawTokens(address token, uint256 amount) external {
-        require(msg.sender == owner, "Only owner");
+
+    function withdrawTokens(address token, uint256 amount) external onlyOwner {
         IERC20(token).transfer(owner, amount);
     }
 }
